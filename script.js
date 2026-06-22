53e791bf87590166f3e01a4581dd686b0b0b3b57
Malu511 <oliveira.santos.maria1706@escola.pr.gov.br>
2026-06-22 12:02:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcular()
+var resultado=document.getElementById ('resultado').innerHTML;
+if (resultado)
+    document.getElementById('resultado').innerHTML=eval (resultado);
+
+
+else
+    document                 ('resultado').       ="nada para calcular";
